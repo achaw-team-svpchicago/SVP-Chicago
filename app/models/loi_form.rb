7b05ab4c59577ceefa19efbc70ea4b1ddd915fa6
MVanderwood <m.b.vanderwood@gmail.com>
2016-02-04 16:22:46 -0600

    Set connection between LoiForm and Answer models and added LoiForm seed data.

--- app/models/loi_form.rb
+++ app/models/loi_form.rb
@@ -1,2 +1,4 @@
 class LoiForm < ActiveRecord::Base
+  has_many :answers
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+form = LoiForm.create({
+  name: Faker::Company.name,
+  email: Faker::Internet.email
+})
+Answer.create({
+  loi_id: form.id,
+  question: "Why is the sky blue?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "Why did the chicken cross the road?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "What is your primary demographic?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "How much blah blah blah blah?",
+  answer: Faker::Hipster.sentence(5)
+})
+form = LoiForm.create({
+  name: Faker::Company.name,
+  email: Faker::Internet.email
+})
+Answer.create({
+  loi_id: form.id,
+  question: "Why is the sky blue?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "Why did the chicken cross the road?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "What is your primary demographic?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "How much blah blah blah blah?",
+  answer: Faker::Hipster.sentence(5)
+})
+form = LoiForm.create({
+  name: Faker::Company.name,
+  email: Faker::Internet.email
+})
+Answer.create({
+  loi_id: form.id,
+  question: "Why is the sky blue?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "Why did the chicken cross the road?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "What is your primary demographic?",
+  answer: Faker::Hipster.sentence(5)
+})
+Answer.create({
+  loi_id: form.id,
+  question: "How much blah blah blah blah?",
+  answer: Faker::Hipster.sentence(5)
+})
