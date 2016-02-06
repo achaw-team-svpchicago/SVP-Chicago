79851381dd3182935ef193093dc844971f872e4a
MVanderwood <m.b.vanderwood@gmail.com>
2016-02-06 14:37:14 -0600

    Set up basic Index and Show views for loi_forms.

--- app/controllers/loi_forms_controller.rb
+++ app/controllers/loi_forms_controller.rb
@@ -1,12 +1,12 @@
 class LoiFormsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
-
+    @loi_forms = LoiForm.all
   end
 
   def show
-    
+    @loi_form = LoiForm.find_by(id: params[:id])
   end
 
   private
--- spec/controllers/loi_forms_controller_spec.rb
+++ spec/controllers/loi_forms_controller_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe LoiFormsController, :type => :controller do
         get :show, id: @loi_form.id
         expect(response).to redirect_to root_path
       end
+
+      it 'does not assign the requested loi_form to @loi_form' do
+        get :show, id: @loi_form.id
+        expect(assigns(:loi_form)).to_not eq @loi_form
+      end
     end
 
     context 'User is an SVP admin' do
@@ -21,9 +26,14 @@ RSpec.describe LoiFormsController, :type => :controller do
       end
 
       it 'renders the show template' do
-        get :show, id: 1
+        get :show, id: @loi_form.id
         expect(response).to render_template :show
       end
+
+      it 'assigns the requested loi_form to @loi_form' do
+        get :show, id: @loi_form.id
+        expect(assigns(:loi_form)).to eq @loi_form
+      end 
     end
 
     context 'User is an SVP super-admin' do
@@ -36,30 +46,47 @@ RSpec.describe LoiFormsController, :type => :controller do
         get :show, id: @loi_form.id
         expect(response).to render_template :show
       end
+
+      it 'assigns the requested loi_form to @loi_form' do
+        get :show, id: @loi_form.id
+        expect(assigns(:loi_form)).to eq @loi_form
+      end 
     end
   end
 
   describe 'GET loi_forms' do
     before :each do
-      @loi_form = FactoryGirl.create(:loi_form)
+      form1 = FactoryGirl.create(:loi_form)
+      form2 = FactoryGirl.create(:loi_form)
+      @loi_forms = [form1, form2]
     end
 
     context 'User is not an SVP partner' do
       it 'redirects to root' do
-        get :index, id: @loi_form.id
+        get :index
         expect(response).to redirect_to root_path
+      end
+
+      it 'does not populate an array of all loi_forms' do
+        get :index
+        expect(assigns(:loi_forms)).to_not match_array @loi_forms
       end
     end
 
     context 'User is an SVP admin' do
       before :each do
         @user = FactoryGirl.create(:admin)
-        sign_in @user            #sign_in is a method from Devise::TestHelpers in 'spec/rails_helper'
+        sign_in @user
       end
 
       it 'renders the index template' do
-        get :index, id: 1
+        get :index
         expect(response).to render_template :index
+      end
+
+      it 'populates an array of all loi_forms' do
+        get :index
+        expect(assigns(:loi_forms)).to match_array @loi_forms
       end
     end
 
@@ -70,8 +97,13 @@ RSpec.describe LoiFormsController, :type => :controller do
       end
 
       it 'renders the index template' do
-        get :index, id: @loi_form.id
+        get :index
         expect(response).to render_template :index
+      end
+
+      it 'populates an array of all loi_forms' do
+        get :index
+        expect(assigns(:loi_forms)).to match_array @loi_forms
       end
     end
   end
