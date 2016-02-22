1d69fe427d6a3692b4b10f29245c394f94f1c09d
Nina Leung <nleung320@gmail.com>
2016-02-21 20:56:57 -0600

    used params hash to save new loi_form object

--- app/controllers/nonprofits_controller.rb
+++ app/controllers/nonprofits_controller.rb
@@ -7,18 +7,27 @@ class NonprofitsController < ApplicationController
   end
 
   def create
-    @loi_form = LoiForm.new(
-      organization: params[:organization],
-      address: params[:address],
-      phone: params[:phone],
-      website: params[:website],
-      executive: params[:executive],
-      executive_phone: params[:executive_phone],
-      executive_email: params[:executive_email],
-      contact: params[:contact],
-      contact_phone: params[:contact_phone],
-      contact_email: params[:contact_email]
-      )
+    loi_form_params = params.to_hash.delete_if do |key, value| 
+      key == "authenticity_token" || key == "controller" || key == "action"
+    end
+    puts "==========================================="
+    puts loi_form_params
+    puts "==========================================="
+
+    @loi_form = LoiForm.new(loi_form_params)
+    # @loi_form = LoiForm.new(
+    #   organization: params[:organization],
+    #   address: params[:address],
+    #   phone: params[:phone],
+    #   website: params[:website],
+    #   executive: params[:executive],
+    #   executive_phone: params[:executive_phone],
+    #   executive_email: params[:executive_email],
+    #   contact: params[:contact],
+    #   contact_phone: params[:contact_phone],
+    #   contact_email: params[:contact_email]
+    #   )
+
     if @loi_form.save
       flash[:success] = "Success! You've successfully submitted a letter of interest to SVP Chicago."
       redirect_to '/'
