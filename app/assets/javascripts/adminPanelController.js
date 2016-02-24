3dab6c555cca9b92dc7095f024cc23f4f4fc3e35
MVanderwood <m.b.vanderwood@gmail.com>
2016-02-23 22:58:46 -0600

    Updated user managment secion of admin panel with buttons to edit user info, delete accounts and manage admin rights (not yet functional).

--- app/assets/javascripts/adminPanelController.js
+++ app/assets/javascripts/adminPanelController.js
@@ -4,8 +4,13 @@
   "use strict";
   angular.module("app").controller("adminPanelController", ["$scope", "$http", function($scope, $http) {
 
-
-    $scope.invitee = {super_admin: false};
+    $scope.setup = function() {
+      $http.get("/api/v1/admin_panel").success(function(response) {
+        $scope.partners = response.partners;
+        $scope.admins = response.admins;
+        $scope.invitee = {super_admin: false};
+      });
+    };
 
     $scope.invitePartner = function(invitee) {
       $http.post("/admin_panel", invitee).success(function() {
