1e5341fdb6e4e02c2e1c9b2d9139d2e72bc94091
MVanderwood <m.b.vanderwood@gmail.com>
2016-03-02 22:32:39 -0600

    Refactored admin panel controllers.

--- app/controllers/api/v1/admin_panel_controller.rb
+++ app/controllers/api/v1/admin_panel_controller.rb
@@ -4,4 +4,49 @@ class Api::V1::AdminPanelController < ApplicationController
     @users = User.all
   end
 
+  def toggle_admin_rights
+    @user = User.find_by(id: params[:id])
+    render json: @user.errors unless @user.update({super_admin: !@user.super_admin})
+  end
+
+  def delete_user
+    @user = User.find_by(id: params[:id])
+
+    begin
+      @user.destroy
+    rescue NoMethodError
+      render json: nil, status: :not_found
+    end
+  end
+
+  def update_user
+    @user = User.find_by(id: params[:id])
+    unless @user.update(params[:admin_panel].permit(:first_name, :last_name, :email))
+      render json: @user.errors, status: :unprocessable_entity 
+    end
+  end  
+
+  def invite_user
+    password = SecureRandom.hex
+    @user = User.new({
+      first_name: params[:firstName],
+      last_name: params[:lastName],
+      email: params[:email],
+      super_admin: params[:super_admin],
+      password: password
+    })
+
+    if @user.save
+      UserMailer.email_invitation(@user, password)
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def resend_activation
+    password = SecureRandom.hex
+    user = User.find_by(id: params[:id])
+    UserMailer.email_invitation(user, password)
+    render json: nil, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,6 @@ Rails.application.routes.draw do
   }
 
   get '/admin_panel' => 'admin_panel#show'
-  post '/admin_panel' => 'admin_panel#invite_user'
-  post '/admin_panel/:id' => 'admin_panel#resend_activation'
-  patch '/admin_panel' => 'admin_panel#update_user'
-  patch '/admin_panel/:id' => 'admin_panel#toggle_admin_rights'
-  delete '/admin_panel/:id' => 'admin_panel#delete_user'
 
   get '/letter_of_interest' => 'nonprofits#loi_form'
   post '/letter_of_interest' => 'nonprofits#create'
@@ -26,6 +21,11 @@ Rails.application.routes.draw do
       post '/loi_forms/:id' => 'loi_forms#create_loi_rating'
 
       get '/admin_panel' => 'admin_panel#show'
+      post '/admin_panel' => 'admin_panel#invite_user'
+      post '/admin_panel/:id' => 'admin_panel#resend_activation'
+      patch '/admin_panel' => 'admin_panel#update_user'
+      patch '/admin_panel/:id' => 'admin_panel#toggle_admin_rights'
+      delete '/admin_panel/:id' => 'admin_panel#delete_user'
     end
   end
 
