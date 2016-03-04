59d7239db61b5aea0427b556595d80ca4ffd6ec4
Nina Leung <nleung320@gmail.com>
2016-03-04 15:43:08 -0600

    changed validations for requested word limits

--- app/models/loi_form.rb
+++ app/models/loi_form.rb
@@ -3,9 +3,10 @@ class LoiForm < ActiveRecord::Base
   has_many :loi_ratings
   validates :organization, presence: true
   validates_length_of :affiliated, maximum: 50, too_long: '- Please limit your response to 50 words.', tokenizer: ->(str) { str.scan(/\w+/) }
-  validates_length_of :mission, maximum: 50, too_long: '- Please limit your response to 50 words.', tokenizer: ->(str) { str.scan(/\w+/) }
-  validates_length_of :vision, maximum: 50, too_long: '- Please limit your response to 50 words.', tokenizer: ->(str) { str.scan(/\w+/) }
-  validates_length_of :concerns, maximum: 50, too_long: '- Please limit your response to 50 words.', tokenizer: ->(str) { str.scan(/\w+/) }
+  validates_length_of :mission, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
+  validates_length_of :vision, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
+  validates_length_of :concerns, maximum: 250, too_long: '- Please limit your response to 250 words.', tokenizer: ->(str) { str.scan(/\w+/) }
+  validates_length_of :additional, maximum: 250, too_long: '- Please limit your response to 250 words.', tokenizer: ->(str) { str.scan(/\w+/) }
 
 
   def rated_by?(user)
