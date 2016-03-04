4801b364993990f72d2857f3a737e33cc23a5a9e
Nina Leung <nleung320@gmail.com>
2016-03-04 16:47:10 -0600

    added integer validations to organization numbers fields

--- app/models/loi_form.rb
+++ app/models/loi_form.rb
@@ -7,6 +7,9 @@ class LoiForm < ActiveRecord::Base
   validates_length_of :vision, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :concerns, maximum: 250, too_long: '- Please limit your response to 250 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :additional, maximum: 250, too_long: '- Please limit your response to 250 words.', tokenizer: ->(str) { str.scan(/\w+/) }
+  validates :employees, numericality: { only_integer: true }
+  validates :volunteers, numericality: { only_integer: true }
+  validates :board, numericality: { only_integer: true }
 
 
   def rated_by?(user)
