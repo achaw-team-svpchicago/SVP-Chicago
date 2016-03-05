b36cb240df76eb9d28ff08395be3448dcd6e8292
Nina Leung <nleung320@gmail.com>
2016-03-04 20:26:34 -0600

    added contact validations

--- app/models/loi_form.rb
+++ app/models/loi_form.rb
@@ -2,6 +2,10 @@ class LoiForm < ActiveRecord::Base
   has_many :answers
   has_many :loi_ratings
   validates :organization, presence: true
+  validates :executive, presence: true
+  validates :executive_email, presence: true
+  validates :contact, presence: true
+  validates :contact_email, presence: true
   validates_length_of :affiliated, maximum: 50, too_long: '- Please limit your response to 50 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :mission, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :vision, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
