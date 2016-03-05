a2310a4030d363b79f11cfbfe3fac8016e155850
Nina Leung <nleung320@gmail.com>
2016-03-04 21:09:21 -0600

    added validations for rest of sections

--- app/models/loi_form.rb
+++ app/models/loi_form.rb
@@ -6,14 +6,17 @@ class LoiForm < ActiveRecord::Base
   validates :executive_email, presence: true
   validates :contact, presence: true
   validates :contact_email, presence: true
+  validates :employees, numericality: { only_integer: true }
+  validates :volunteers, numericality: { only_integer: true }
+  validates :board, numericality: { only_integer: true }
+  validates :budget, presence: true
   validates_length_of :affiliated, maximum: 50, too_long: '- Please limit your response to 50 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :mission, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :vision, maximum: 500, too_long: '- Please limit your response to 500 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :concerns, maximum: 250, too_long: '- Please limit your response to 250 words.', tokenizer: ->(str) { str.scan(/\w+/) }
   validates_length_of :additional, maximum: 250, too_long: '- Please limit your response to 250 words.', tokenizer: ->(str) { str.scan(/\w+/) }
-  validates :employees, numericality: { only_integer: true }
-  validates :volunteers, numericality: { only_integer: true }
-  validates :board, numericality: { only_integer: true }
+  validates :sectors, presence: true
+  validates :geo_focus, presence: true
 
 
   def rated_by?(user)
